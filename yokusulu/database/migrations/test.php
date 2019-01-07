828217c0c6960ee595dd9561821e6c50a3ec1bee
Shunsuke Hayashi <cc89mk@gmail.com>
2019-01-07 18:02:14 +0900

    ブランチあってるか確認お願いします！

---
+++ yokusulu/database/migrations/test.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->unsigned('id')->increments('id')->primary('id');
+            $table->string('name', 50)->nullable(false);
+            $table->string('email', 100)->nullable(false);
+            $table->string('password', 100)->nullable(false);
+            $table->integer('del_flg', 1)->default($0);
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+
+        Schema::create('host_users', function (Blueprint $table) {
+            $table->unsigned('id')->increments('id')->primary('id');
+            $table->integer('phone', 11)->nullable(false);
+            $table->integer('zip', 7)->nullable(false);
+            $table->string('prefecture', 10)->nullable(false);
+            $table->string('city', 30)->nullable(false);
+            $table->string('ward', 30)->nullable(false);
+            $table->string('address', 30)->nullable(false);
+            $table->integer('del_flg', 1)->default($0);
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+
+        Schema::create('super_users', function (Blueprint $table) {
+          $table->unsigned('id')->increments('id',10)->primary('id');
+          $table->string('name', 50)->nullable(false);
+          $table->string('email', 100)->nullable(false);
+          $table->string('password', 100)->nullable(false);
+          $table->integer('del_flg', 1)->default($0);
+          $table->timestamps('created_at');
+          $table->timestamps('updated_at');
+
+        Schema::create('houses', function (Blueprint $table) {
+          $table->unsigned('id')->increments('id')->primary('id');
+          $table->string('name', 50)->nullable(false);
+          $table->string('host_name', 50)->nullable(false);
+          $table->integer('zip', 7)->nullable(false);
+          $table->string('prefecture', 10)->nullable(false);
+          $table->string('city', 30)->nullable(false);
+          $table->string('ward', 30)->nullable(false);
+          $table->string('address', 30)->nullable(false);
+          $table->integer('price', 8)->nullable(false);
+          $table->integer('del_flg', 1)->default($0);
+          $table->timestamps('created_at');
+          $table->timestamps('updated_at');
+
+        Schema::create('detail_houses', function (Blueprint $table) {
+          $table->unsigned('id')->increments('id')->primary('id');
+          $table->integer('houses_id', 10)->nullable(false);
+          $table->text('description')->nullable(false);
+          $table->integer('del_flg', 1)->default($0);
+          $table->timestamps('created_at');
+          $table->timestamps('updated_at');
+
+        Schema::create('disable_booking_dates', function (Blueprint $table) {
+          $table->unsigned('id')->increments('id')->primary('id');
+          $table->integer('houses_id', 10)->nullable(false);
+          $table->integer('year', 4)->nullable(false);
+          $table->integer('month', 2)->nullable(false);
+          $table->integer('day', 2)->nullable(false);
+
+        Schema::create('house_images', function (Blueprint $table) {
+          $table->unsigned('id')->increments('id',10)->primary('id');
+          $table->integer('houses_id', 10)->nullable(false);
+          $table->text('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
