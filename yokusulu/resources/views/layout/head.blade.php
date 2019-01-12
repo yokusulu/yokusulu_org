bed9b9b1f9c527502e8d102bbf289f90c5dee94e
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-12 12:50:43 +0900

    sassビルドして反映確認
    css読み込み順序変更

--- yokusulu/resources/views/layout/head.blade.php
+++ yokusulu/resources/views/layout/head.blade.php
@@ -2,9 +2,9 @@
 <meta charset="UTF-8">
 <title>@yield('title') | yokusulu</title>
 <meta name="description" itemprop="description" content="@yield('description')">
-<link rel="stylesheet" href="{{ asset('/css/common.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/base.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
-<link rel="stylesheet" href="{{ asset('/css/base.css') }}">
 @yield('pageCss')
 @endsection
