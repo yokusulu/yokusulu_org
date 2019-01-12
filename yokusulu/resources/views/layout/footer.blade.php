07fe1fb0aa276895b4ff6c6284fe8e799792705f
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-12 15:20:11 +0900

    sassテスト

--- yokusulu/resources/views/layout/footer.blade.php
+++ yokusulu/resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 @section('footer')
 <footer class="footer">
-    フッター
+    <p>&copy; 2019 YOKUSULU</p>
 </footer>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 @yield('pageJs')
--- yokusulu/resources/views/layout/head.blade.php
+++ yokusulu/resources/views/layout/head.blade.php
@@ -3,6 +3,7 @@
 <title>@yield('title') | yokusulu</title>
 <meta name="description" itemprop="description" content="@yield('description')">
 <link rel="stylesheet" href="{{ asset('/css/base.css') }}">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
