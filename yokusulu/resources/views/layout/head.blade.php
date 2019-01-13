66ecf5f8498bc7bddde829fe03720fd2ca894701
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-13 15:52:26 +0000

    sassテスト

--- yokusulu/resources/views/layout/head.blade.php
+++ yokusulu/resources/views/layout/head.blade.php
@@ -5,7 +5,5 @@
 <link rel="stylesheet" href="{{ asset('/css/base.css') }}">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-<link rel="stylesheet" href="{{ asset('/css/header.css') }}">
-<link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
 @yield('pageCss')
 @endsection
--- yokusulu/resources/views/layout/header.blade.php
+++ yokusulu/resources/views/layout/header.blade.php
@@ -1,5 +1,11 @@
 @section('header')
 <header class="header">
-    ヘッダー
+    <div class="logo">
+        <img src="" alt="">
+    </div>
+    <ul>
+        <li><button type="button" class="btn btn-primary">ログアウト</button></li>
+        <li><button type="button" class="btn btn-primary">ログイン</button></li>
+    </ul>
 </header>
 @endsection
