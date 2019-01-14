0f28c65e2a94206fb890aad89e8dd3e717d6a6d3
shun <ringorian0626@gmail.com>
2019-01-14 22:43:05 +0900

    vue使う準備、矢島環境仕上がり次第本番にのせてみます

--- yokusulu/resources/views/layout/footer.blade.php
+++ yokusulu/resources/views/layout/footer.blade.php
@@ -1,6 +1,7 @@
 @section('footer')
 <footer class="footer">
-    <p>&copy; 2019 YOKUSULU</p>
+	<p>&copy; 2019 YOKUSULU</p>
+	<script src=" {{ mix('js/app.js') }} "></script>
 </footer>
 @yield('pageJs')
 @endsection
