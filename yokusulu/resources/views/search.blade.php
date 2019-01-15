cde425bd77f890b70fb3cb2c121f1fdc192c8a4e
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-15 14:59:00 +0000

    saving

--- yokusulu/resources/views/search.blade.php
+++ yokusulu/resources/views/search.blade.php
@@ -11,8 +11,39 @@
 @include('layout.header')
 
 @section('content')
-<div class="searchList">
+<div class="list">
     <h1>検索結果</h1>
+    <div class="listItem fade-up">
+        <div class="listItem__img">
+            <img src="https://tools.arashichang.com/300x200/cccccc/ffffff" />
+        </div>
+        <p>consectetur adipiscing elit. Vivamus consectetur odio in felis vulputate, a viverra mi molestie. Suspendisse vehicula ligula turpis, nec dapibus tellus aliquam et. Vivamus luctus luctus libero, non placerat nisl
+            feugiat nec. Duis ultrices enim in dolor egestas bibendum eget non urna. Fusce consequat accumsan mauris ut sollicitudin. Donec lorem nisl, laoreet non urna at, feugiat feugiat dui. Fusce dignissim dolor vel magna lobortis, sit amet</p>
+    </div>
+
+    <div class="listItem fade-up">
+        <div class="listItem__img">
+            <img src="https://tools.arashichang.com/300x200/cccccc/ffffff" />
+        </div>
+        <p>consectetur adipiscing elit. Vivamus consectetur odio in felis vulputate, a viverra mi molestie. Suspendisse vehicula ligula turpis, nec dapibus tellus aliquam et. Vivamus luctus luctus libero, non placerat nisl
+            feugiat nec. Duis ultrices enim in dolor egestas bibendum eget non urna. Fusce consequat accumsan mauris ut sollicitudin. Donec lorem nisl, laoreet non urna at, feugiat feugiat dui. Fusce dignissim dolor vel magna lobortis, sit amet</p>
+    </div>
+
+    <div class="listItem fade-up">
+        <div class="listItem__img">
+            <img src="https://tools.arashichang.com/300x200/cccccc/ffffff" />
+        </div>
+        <p>consectetur adipiscing elit. Vivamus consectetur odio in felis vulputate, a viverra mi molestie. Suspendisse vehicula ligula turpis, nec dapibus tellus aliquam et. Vivamus luctus luctus libero, non placerat nisl
+            feugiat nec. Duis ultrices enim in dolor egestas bibendum eget non urna. Fusce consequat accumsan mauris ut sollicitudin. Donec lorem nisl, laoreet non urna at, feugiat feugiat dui. Fusce dignissim dolor vel magna lobortis, sit amet</p>
+    </div>
+    
+    <div class="listItem fade-up">
+        <div class="listItem__img">
+            <img src="https://tools.arashichang.com/300x200/cccccc/ffffff" />
+        </div>
+        <p>consectetur adipiscing elit. Vivamus consectetur odio in felis vulputate, a viverra mi molestie. Suspendisse vehicula ligula turpis, nec dapibus tellus aliquam et. Vivamus luctus luctus libero, non placerat nisl
+            feugiat nec. Duis ultrices enim in dolor egestas bibendum eget non urna. Fusce consequat accumsan mauris ut sollicitudin. Donec lorem nisl, laoreet non urna at, feugiat feugiat dui. Fusce dignissim dolor vel magna lobortis, sit amet</p>
+    </div>
 </div>
 @endsection
 
