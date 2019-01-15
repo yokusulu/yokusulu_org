db7b0b1b1ea3c6c43fa9e288b361ef82c87f5a9e
hiroki <jazzmusixbass.sm.p@gmail.com>
2019-01-15 11:45:36 +0900

    bootstrapでheader基盤作成

--- yokusulu/resources/views/layout/header.blade.php
+++ yokusulu/resources/views/layout/header.blade.php
@@ -1,11 +1,23 @@
 @section('header')
 <header class="header">
-    <div class="logo">
-        <img src="" alt="">
-    </div>
-    <ul>
-        <li><button type="button" class="btn btn-primary">ログアウト</button></li>
-        <li><button type="button" class="btn btn-primary">ログイン</button></li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="" width="100" height="30" alt="yokusulu">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
 @endsection
