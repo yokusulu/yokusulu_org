86a9f3102ab19bb5061c1f42352892724cb21fbc
hiroki <jazzmusixbass.sm.p@gmail.com>
2019-01-15 18:37:14 +0900

    style修正。js読み込み位置修正

--- yokusulu/resources/views/layout/header.blade.php
+++ yokusulu/resources/views/layout/header.blade.php
@@ -1,20 +1,18 @@
 @section('header')
 <header class="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <img src="" width="100" height="30" alt="yokusulu">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">YOKUSULU</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
+                    <a class="nav-link text-light" href="#">Logout</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link text-light" href="#">Login</a>
                 </li>
             </ul>
         </div>
