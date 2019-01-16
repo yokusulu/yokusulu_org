7a8f96e69bd6601698f1203f8c92359a41dd98c2
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-16 14:47:03 +0000

    headerの色味変更

--- yokusulu/resources/views/layout/header.blade.php
+++ yokusulu/resources/views/layout/header.blade.php
@@ -1,6 +1,6 @@
 @section('header')
 <header class="header">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="#">YOKUSULU</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
