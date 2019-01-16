a539e0ecbdaf08605cd7046bad72e05a2ccda42c
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-16 14:08:03 +0000

    saving

--- yokusulu/resources/views/search.blade.php
+++ yokusulu/resources/views/search.blade.php
@@ -36,7 +36,7 @@
         <p>consectetur adipiscing elit. Vivamus consectetur odio in felis vulputate, a viverra mi molestie. Suspendisse vehicula ligula turpis, nec dapibus tellus aliquam et. Vivamus luctus luctus libero, non placerat nisl
             feugiat nec. Duis ultrices enim in dolor egestas bibendum eget non urna. Fusce consequat accumsan mauris ut sollicitudin. Donec lorem nisl, laoreet non urna at, feugiat feugiat dui. Fusce dignissim dolor vel magna lobortis, sit amet</p>
     </div>
-    
+
     <div class="listItem fade-up">
         <div class="listItem__img">
             <img src="https://tools.arashichang.com/300x200/cccccc/ffffff" />
