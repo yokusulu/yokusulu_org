e175b57fcaaa9b0c8ba021d28f7b544abf89febc
shun <ringorian0626@gmail.com>
2019-01-19 21:50:16 +0900

    Disable_booking_datesTableのぶん

--- yokusulu/database/seeds/DatabaseSeeder.php
+++ yokusulu/database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
 		$this->call(Host_usersTableSeeder::class);
 		$this->call(Super_usersTableSeeder::class);
 		$this->call(HousesTableSeed::class);
+		$this->call(Disable_booking_datesTableSeeder::class);
 	}
 }
