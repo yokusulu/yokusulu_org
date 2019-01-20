eb29001d46a1e02373a5a0e9ef0ce31326ac058b
shun <ringorian0626@gmail.com>
2019-01-20 16:29:20 +0900

    House_imagesテーブルぶん

--- yokusulu/database/seeds/DatabaseSeeder.php
+++ yokusulu/database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
 		$this->call(Super_usersTableSeeder::class);
 		$this->call(HousesTableSeed::class);
 		$this->call(Disable_booking_datesTableSeeder::class);
+		$this->call(House_imagesTableSeeder::class);
 	}
 }
