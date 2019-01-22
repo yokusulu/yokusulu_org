ef555f80c21f662178d77d2f0fcc119d0a183146
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-22 23:06:28 +0900

    マイグレーション復活の呪文

---
+++ yokusulu/database/migrations/2019_01_19_064039_create_house_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHouseImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('house_images', function (Blueprint $table) {
+            $table->increments('id')->nullable(false);
+            $table->unsignedInteger('houses_id')->nullable(false);
+            $table->binary('images');
+            $table->timestamps();
+            $table->foreign('houses_id')->references('id')->on('houses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('house_images');
+    }
+}
