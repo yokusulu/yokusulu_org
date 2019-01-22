8022f09494461f359ac78a0811b99dd5740f49c6
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-22 23:54:18 +0900

    search.sassのフォルダ階層修正

--- yokusulu/webpack.mix.js
+++ yokusulu/webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .sass('resources/sass/search.scss', 'public/css');
+   .sass('resources/sass/search/search.scss', 'public/css/search');
