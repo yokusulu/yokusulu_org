026f35ab06fcf0fbc9ddd803966a1aafb625179c
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-01-23 23:45:08 +0900

    saving

--- yokusulu/resources/views/home.blade.php
+++ yokusulu/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.common')
 
-@section('title', 'hoge')
-@section('description', '説明文')
+@section('title', 'HOME')
+@section('description', '空いたスペースを共有してWINWINな体験を。')
 @section('pageCss')
-<link href="/css/page.css" rel="stylesheet">
+<link href="/css/home.css" rel="stylesheet">
 @endsection
 
 @include('layout.head')
@@ -11,7 +11,14 @@
 @include('layout.header')
 
 @section('content')
-    <p>コンテンツ内容</p>
+    <div class="container">
+        <div class="search_box">
+            <form action="">
+                
+            </form>
+        </div>
+
+    </div>
 @endsection
 
 @section('pageJs')
