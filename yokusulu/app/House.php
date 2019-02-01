55ef2e7820e55cb20dd1d702bf9c5bfa811e5745
shun <ringorian0626@gmail.com>
2019-02-02 01:34:58 +0900

    林、がんばろうや

--- yokusulu/app/House.php
+++ yokusulu/app/House.php
@@ -3,10 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class House extends Model
 {
+    private $house = 'houses';
     //
-    protected $house = 'houses_table';
+    // public function __construct()
+    // {
+    //     $this->$foo = TRUE;
 
+    //     echo($this->$foo);
+    // }
+
+    public static function get_data($house_id) {
+    	// $data = new House;
+    	 var_dump($this); die;
+    	// var_dump($this->$house); die;
+    $house_data = DB::table($house)->join('detail_houses', 'houses.id', '=', 'detail_houses.houses_id')->distinct()->get();
+    var_dump($house_data); die;
+   	 return $house;
+    }
+
+    // public static function a() {
+    // 	echo '成功'; die;
+    // 	 return $house;
+    // }
 }
--- yokusulu/app/Http/Controllers/SearchController.php
+++ yokusulu/app/Http/Controllers/SearchController.php
@@ -5,24 +5,30 @@ namespace App\Http\Controllers;
 use App\House;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 
 class SearchController extends Controller
 {
     //
-    protected $members;
+    // protected $members;
 
-    public function __construct()
-    {
-        $this->houses = new House();
-    }
+    // public function __construct()
+    // {
+    //     $this->houses = new House();
+    // }
 
     public function detail_index() {
-        $houses = $this->houses->all();
-        foreach ($houses as $house){
-            echo $house -> name;
-        }
+        // $user = new \App\User;
+        $house_id = "1";
+        $house = House::get_data($house_id);
+        var_dump($house);
+        // $user->email = $post["email"];
+        // $houses = $this->houses->all();
+        // foreach ($houses as $house){
+        //     echo $house -> name;
+        // }
 
         return view("search/detail/index")->with([
         ]);
