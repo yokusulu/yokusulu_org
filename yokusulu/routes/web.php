fcdd6fa86db3c56fde3319d3f08ecb365f8802b5
HirokiNakamura <jazzmusixbass.sm.p@gmail.com>
2019-02-07 23:33:30 +0900

    make:auth実行

--- yokusulu/routes/web.php
+++ yokusulu/routes/web.php
@@ -13,3 +13,7 @@
 
 Route::get('/', function () {    return view('welcome');});
 Route::get('sample', 'SamplesController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
